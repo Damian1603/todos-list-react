448fde34cf54668e1cf076226975d4f1272194d2
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-06-30 14:12:59 +0200

    task list without interactions yet

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "./style.css"
 
-const Buttons = ({ tasks, hideCompleted }) => {
-    if (tasks.length === 0) {
-        return null
-    }
+const Buttons = ({ tasks, hideCompleted }) => (
 
-    return (
-        <div className="section__headerButtons">
-            <button className={`section__headerButtons`}>
-                {hideCompleted ? "Pokaż ukończone" : "Ukryj ukończone"}
-            </button>
-            <button
-                className="section__headerButtons"
-                disabled={tasks.every(({ done })=> done)}
-            >
-                Ukończ wszystkie
-            </button>
-        </div >
-    )
-};
-
+    <div className="section__headerButtons">
+        {tasks.length > 0 && (
+            <>
+                <button className={`section__headerButtons`}>
+                    {hideCompleted ? "Pokaż ukończone" : "Ukryj ukończone"}
+                </button>
+                <button
+                    className="section__headerButtons"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button>
+            </>
+        )}
+    </div>
+);
 export default Buttons;
