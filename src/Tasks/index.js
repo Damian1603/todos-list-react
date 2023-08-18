52b4fc06462cd21c0977f567b8f5ecd8abe03cd7
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-08-18 19:11:08 +0200

    added line-through to  task done

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,15 +1,15 @@
 import "./style.css";
 
 
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideCompleted }) => (
     <ul className="taskList">
-        {props.tasks.map(task => (
+        {tasks.map(task => (
             <li
-                className={`taskList__item${task.done && props.hideCompleted ? "taskList__item--hidden" : ""}`}>
+                key={task.id} className={`taskList__item${task.done && hideCompleted ? "taskList__item--hidden" : ""}`}>
                 <button className="button">
                     {task.done ? "✓" : ""}
                 </button>
-                <span className={`task.done ? "taskList__done" : ""`}>
+                <span className={`taskList ${task.done ? "taskList__done" : ""}`}>
                     {task.content}
                 </span>
                 <button className="button button--remove">
