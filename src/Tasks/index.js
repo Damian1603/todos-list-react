70b2259e993d0e43819bbb1c944cce30db798049
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-09-04 08:41:07 +0200

    added key to tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,22 @@
 import "./style.css";
 
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone}) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="taskList">
         {tasks.map(task => (
-            <li
-                 className={`tasks__item ${task.done && hideDone ? "tasks__item--hidden" : ""}`}>
-                <button 
-                onClick = {() => toggleTaskDone(task.id)}
-                className="button">
+            <li key={task.id}
+                className={`tasks__item ${task.done && hideDone ? "tasks__item--hidden" : ""}`}>
+                <button
+                    onClick={() => toggleTaskDone(task.id)}
+                    className="button">
                     {task.done ? "✓" : ""}
                 </button>
                 <span className={`task__content ${task.done ? "taskList__done" : ""}`}>
                     {task.content}
                 </span>
-                <button 
-                onClick = {() => removeTask(task.id)}
-                className="button button--remove">
+                <button
+                    onClick={() => removeTask(task.id)}
+                    className="button button--remove">
                     🗑
                 </button>
             </li>
