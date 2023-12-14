1ea107c991232e984c29f800a7f9c2e2bcfeb334
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-14 12:02:42 +0100

    created theme file, added theme provider and global  styled to app

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+    breakpoints:{
+        mobile:767
+    },
+    colors:{
+        white:"#ffffff",
+        green:"#008000",
+        lightGreen:"#009900",
+        activeGreen:"#00b300",
+        crimson:"#dc143c",
+        lightCrimson:"#ea1f48",
+        activeCrimson:"#ec365b",
+        lightTeal:"#009999",
+        teal:"#008080",
+        activeTeal:"#00b3b3"
+    }
+};
