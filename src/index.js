1768c82b937d37acef3b2feb1292cb1727c20ab3
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-16 17:21:37 +0100

    update app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import { GlobalStyled } from './globalStyled';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App>
-        <GlobalStyled />
-      </App>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
