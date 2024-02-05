a948ba7c1ae26f5d83d88c943c0458f481cec930
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-02-05 12:37:44 +0100

    created new reducers in tasks slice file and exported them

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -7,15 +7,28 @@ const tasksSlice = createSlice({
         hideDone: false,
     },
     reducers: {
-        addTask: ({ tasks }, { payload }) => {
-            tasks.push(payload)
+        addTask: ({ tasks }, { payload: task }) => {
+            tasks.push(task)
         },
         toogleHideDone: state => {
             state.hideDone = !state.hideDone
-        }
+        },
+        toogleTaskDone: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks[index].done = !tasks[index].done;
+        },
+        removeTask: ({ tasks }, { payload: taskId }) => {
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks.splice(index, 1)
+        },
+        setAllDone: ({ tasks }) => {
+            for (const task of tasks) {
+                task.done = true
+            }
+        },
     },
 });
 
-export const { addTask, toogleHideDone } = tasksSlice.actions;
+export const { addTask, toogleHideDone, toogleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
