eee0dc61783345f6928cfc2d5b78e3ee336242fd
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-02-06 08:50:19 +0100

    I used dispatch to add tasks based to the addTask reducer

--- src/features/tasks/Form/index.js
+++ src/features/tasks/Form/index.js
@@ -1,10 +1,14 @@
 import { useState, useRef } from "react";
+import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { addTask } from '../tasksSlice';
 import { StyledForm, StyledInput, StyledButton } from "./styled";
 
-export const Form = ({ addNewTask }) => {
+export const Form = () => {
 
     const [newTaskContent, setNewTaskContent] = useState("");
     const inputRef = useRef(null);
+    const dispatch = useDispatch();
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -14,7 +18,11 @@ export const Form = ({ addNewTask }) => {
         if (!trimmedNewTask) {
             return
         }
-        addNewTask(newTaskContent.trim());
+        dispatch(addTask({
+            content: newTaskContent.trim(),
+            done: false,
+            id: nanoid(),
+        }))
         setNewTaskContent("");
         inputRef.current.focus();
     }
