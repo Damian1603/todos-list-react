3ff099da81a25a8d2503742398a81df4fe323686
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-02-06 08:52:35 +0100

    created task list component and changed action after press buttons based dispatch and reducers

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,25 +1,38 @@
-import { StyledList, StyledItem, StyledContent, StyledButton, StyledButtonRemove } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toogleTaskDone, removeTask } from "../tasksSlice";
+import {
+    StyledList,
+    StyledItem,
+    StyledContent,
+    StyledButton,
+    StyledButtonRemove,
+} from "./styled";
 
-export const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <StyledList>
-        {tasks.map(task => (
-            <StyledItem
-                key={task.id}
-                hidden={task.done && hideDone}>
-                <StyledButton
-                    onClick={() => toggleTaskDone(task.id)}
-                >
-                    {task.done ? "✓" : ""}
-                </StyledButton>
-                <StyledContent done={task.done}>
-                    {task.content}
-                </StyledContent>
-                <StyledButtonRemove
-                    onClick={() => removeTask(task.id)}
-                >
-                    🗑
-                </StyledButtonRemove>
-            </StyledItem>
-        ))}
-    </StyledList>
-);
+export const TaskList = () => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch();
+
+    return (
+        <StyledList>
+            {tasks.map(task => (
+                <StyledItem
+                    key={task.id}
+                    hidden={task.done && hideDone}>
+                    <StyledButton
+                        onClick={() => dispatch(toogleTaskDone(task.id))}
+                    >
+                        {task.done ? "✓" : ""}
+                    </StyledButton>
+                    <StyledContent done={task.done}>
+                        {task.content}
+                    </StyledContent>
+                    <StyledButtonRemove
+                        onClick={() => dispatch(removeTask(task.id))}
+                    >
+                        🗑
+                    </StyledButtonRemove>
+                </StyledItem>
+            ))}
+        </StyledList>
+    )
+}
