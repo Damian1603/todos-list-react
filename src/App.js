34c8e5b02da999aa43ef35d34ff78ca2754fda33
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-02-23 17:53:45 +0100

    added hash router and author section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import { Tasks } from "./features/tasks/Tasks";
+import { HashRouter, Link, Switch, Route } from "react-router-dom";
+import { Tasks } from "./features/tasks/Tasks.js";
+import Author from "./features/author/Author";
 
-export const App = () =>
-    <Tasks />
-;
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
