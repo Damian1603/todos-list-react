4ae57f5d2b6d506dcd6363741c3cc8e6928c435c
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-05-02 15:46:58 +0200

    added punctation mark

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -46,7 +46,7 @@ export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, 
 const selectTasksState = state => state.tasks
 
 export const selectTasks = state => selectTasksState(state).tasks;
-export const selectTasksLoading = state => selectTasksState(state).loading
+export const selectTasksLoading = state => selectTasksState(state).loading;
 export const selectHideDone = state => selectTasksState(state).hideDone;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
